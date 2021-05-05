b9f6fc175ec309b00b7e3da5a503b5c4ab65da6a
MAYANK TYAGI <34006835+tmayank860@users.noreply.github.com>
2021-05-05 18:49:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ clock = () => {
   var min = date.getMinutes();
   var sec = date.getSeconds();
 
-  var hour = (hr<9)? hr:"0"+ hr;
+  var hour = (hr>9)? hr:"0"+ hr;
   document.getElementById("hr").innerHTML= hour>12?-24+hour+12:hour;
   document.getElementById("min").innerHTML = min>9? min:"0"+min;
   document.getElementById("sec").innerHTML = sec>9?sec:"0"+sec;
